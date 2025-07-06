caf32378e70127c8e6699f28148cb0564ac554de
shinku <jsgxx00@gmail.com>
2025-07-06 20:10:23 +0800

    update config

--- src/configuration.ts
+++ src/configuration.ts
@@ -15,16 +15,18 @@ export class MainConfiguration implements ILifeCycle {
   async onConfigLoad() {
     // 生产上从 /config/config.json 中读取配置
     const configPath = join('/', 'config/config.json');
+    console.log('dataSource', this.app.config.sequelize.dataSource.default);
     if (existsSync(configPath)) {
       const configData = readFileSync(configPath, 'utf-8');
       const configJson = JSON.parse(configData);
-      const sequelizeConfig = configJson.sequelize;
+      const sequelizeConfig = configJson.sequelize || {};
       Object.assign(this.app.config, configJson);
       this.app.config.sequelize.dataSource.default = {
-        ...this.app.config.sequelize.dataSource.default,
+        ...(this.app.config.sequelize.dataSource.default || {}),
         ...sequelizeConfig,
       };
     }
+    console.log('Config', this.app.config);
   }
   async onReady() {}
 }
